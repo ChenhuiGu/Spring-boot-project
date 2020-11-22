ab7ad4e0045b5f1df9262a338c430c712bfe7a9d
ChenhuiGu <guchh@pbcsf.tsinghua.edu.cn>
2020-11-22 18:10:49 +0800

    beans for annotation

--- Spring5-demo1/src/pers/guchh/dao/userDaoimpl.java
+++ Spring5-demo1/src/pers/guchh/dao/userDaoimpl.java
@@ -1,5 +1,9 @@
 package pers.guchh.dao;
 
+import org.springframework.stereotype.Component;
+
+
+@Component(value = "userDaoimpl")
 public class userDaoimpl implements userDao{
     @Override
     public void update() {
--- Spring5-demo1/src/pers/guchh/service/userService.java
+++ Spring5-demo1/src/pers/guchh/service/userService.java
@@ -1,10 +1,15 @@
 package pers.guchh.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import pers.guchh.dao.userDao;
 
 
+@Component
 public class userService {
     //为什么不用userDaoimpl
+    @Autowired
     private userDao userdao;
 
     public void setUserdao(userDao userdao) {
